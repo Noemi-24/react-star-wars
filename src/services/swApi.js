f997229baa7b6bf37fa06d233e262d818bad8d3c
Noemi Roldan <noemi.roldan@peopleshores.com>
2026-02-19 12:04:38 -0500

    first commit

---
+++ src/services/swApi.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const BASE_URL = 'https://swapi.dev/api/';
+
+const api = axios.create({
+    baseURL: BASE_URL,
+});
+
+export const getStarships = async () =>{
+    try {
+    const response = await api.get('/starships'); 
+    return response.data; // The actual data is in the 'data' property of the response
+  } catch (error) {
+    console.error('Error fetching breeds:', error);
+    throw error; // Propagate the error for the calling function to handle
+  }
+}
+
+export const getStartshipById = async (starshipId) =>{
+    try {
+        const response =  await api.get('/starships/', {
+            params:{
+                id: starshipId,
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching breeds:', error);
+        throw error; 
+    }
+}
+
